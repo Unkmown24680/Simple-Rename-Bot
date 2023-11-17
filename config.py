14b50de11bdb697a1a7e31dd63f2dac031e65470
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-17 23:11:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "23576751"))
+API_HASH = environ.get("API_HASH", "5224da0df5c60ec53de09ce79b9aa2c3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6753542978:AAE3UEorQCTA5C0BlhbqiHfhjrn3gV3N1Gs")
+ADMIN = int(environ.get("ADMIN", "6642501643"))          
+CAPTION = environ.get("CAPTION", "join @movierulzking")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
